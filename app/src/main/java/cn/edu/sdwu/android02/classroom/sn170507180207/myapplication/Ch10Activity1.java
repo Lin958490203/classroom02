0a289d9d1dc4a48d7860d15699a467750e9c4837
Lin958490203 <958490203@qq.com>
2020-04-22 13:26:38 +0800

    change

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180207/myapplication/Ch10Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180207/myapplication/Ch10Activity1.java
@@ -1,11 +1,13 @@
 package cn.edu.sdwu.android02.classroom.sn170507180207.myapplication;
 
+import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Interpolator;
+import android.widget.TextView;
 
 public class Ch10Activity1 extends AppCompatActivity {
     private Integer count;//点击按键的计数器
@@ -15,6 +17,14 @@ public class Ch10Activity1 extends AppCompatActivity {
         Log.i(Ch10Activity1.class.toString(),"onCreate");
         setContentView(R.layout.layout_ch10_1);
         count=0;
+
+        //接受数据
+        Intent intent=getIntent();//获取界面跳转时使用的intent
+        String text=intent.getStringExtra("text");
+        TextView textView=(TextView)findViewById(R.id.ch10_1_tv);
+        textView.setText(text);
+
+
     }
 
     @Override
